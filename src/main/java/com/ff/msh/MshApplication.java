4049e2fa7b633bd5ee483cfad46a4ea5b28b9343
Florian <florian@crewmeister.com>
2021-12-19 09:00:19 +0100

    Initial commit

---
+++ src/main/java/com/ff/msh/MshApplication.java
@@ -0,0 +1,13 @@
+package com.ff.msh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MshApplication.class, args);
+	}
+
+}
